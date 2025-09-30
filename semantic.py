64a061bed981650e827e62b3ab0c37df8362e795
Piotr <piotrek124@hotmail.com>
2025-09-30 12:16:49 +0200

    UNet semantic model

--- semantic.py
+++ semantic.py
@@ -10,8 +10,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-EPOCHS = 200
+EPOCHS = 100
 BATCH_SIZE = 1
+IMAGE_SIZE = 512
 
 # Classes
 class Data(Dataset):
@@ -41,7 +42,7 @@ class Data(Dataset):
             mask = self.mask_transform(mask)
 
         mask = np.array(mask)
-        red_threshold = (mask[:, :, 0] > 150) & (mask[:, :, 1] < 100) & (mask[:, :, 2] < 100)
+        red_threshold = (mask[:, :, 0] > 100) & (mask[:, :, 1] < 100) & (mask[:, :, 2] < 100)
         mask_class = np.zeros((mask.shape[0], mask.shape[1]), dtype=np.int64)
         mask_class[red_threshold] = 1
         mask = torch.from_numpy(mask_class)
@@ -66,57 +67,73 @@ class DoubleConv(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
-class UNet(nn.Module):
-    def __init__(self, n_classes):
-        super(UNet, self).__init__()
-        # Encoder
-        self.enc1 = DoubleConv(3, 64)
-        self.pool1 = nn.MaxPool2d(2)
-        self.enc2 = DoubleConv(64, 128)
-        self.pool2 = nn.MaxPool2d(2)
-
-        # Bottleneck
-        self.bottleneck = DoubleConv(128, 256)
-
-        # Decoder
-        self.up1 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
-        self.dec1 = DoubleConv(256, 128)
-        self.up2 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
-        self.dec2 = DoubleConv(128, 64)
-
-        # Output
-        self.out_conv = nn.Conv2d(64, n_classes, kernel_size=1)
+class DownSample(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv = DoubleConv(in_channels, out_channels)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
     def forward(self, x):
-        # Encoder
-        e1 = self.enc1(x)
-        e2 = self.enc2(self.pool1(e1))
+        down = self.conv(x)
+        p = self.pool(down)
 
-        # Bottleneck
-        b = self.bottleneck(self.pool2(e2))
+        return down, p
 
-        # Decoder
-        d1 = self.up1(b)
-        d1 = torch.cat([d1, e2], dim=1)
-        d1 = self.dec1(d1)
+class UpSample(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.up = nn.ConvTranspose2d(in_channels, in_channels//2, kernel_size=2, stride=2)
+        self.conv = DoubleConv(in_channels, out_channels)
 
-        d2 = self.up2(d1)
-        d2 = torch.cat([d2, e1], dim=1)
-        d2 = self.dec2(d2)
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        x = torch.cat([x1, x2], 1)
+        return self.conv(x)
 
-        return self.out_conv(d2)
+class UNet(nn.Module):
+    def __init__(self, n_classes):
+        super().__init__()
+        self.down_convolution_1 = DownSample(3, 64)
+        self.down_convolution_2 = DownSample(64, 128)
+        self.down_convolution_3 = DownSample(128, 256)
+        self.down_convolution_4 = DownSample(256, 512)
+
+        self.bottle_neck = DoubleConv(512, 1024)
+
+        self.up_convolution_1 = UpSample(1024, 512)
+        self.up_convolution_2 = UpSample(512, 256)
+        self.up_convolution_3 = UpSample(256, 128)
+        self.up_convolution_4 = UpSample(128, 64)
+
+        self.out = nn.Conv2d(in_channels=64, out_channels=n_classes, kernel_size=1)
+
+    def forward(self, x):
+        down_1, p1 = self.down_convolution_1(x)
+        down_2, p2 = self.down_convolution_2(p1)
+        down_3, p3 = self.down_convolution_3(p2)
+        down_4, p4 = self.down_convolution_4(p3)
+
+        b = self.bottle_neck(p4)
+
+        up_1 = self.up_convolution_1(b, down_4)
+        up_2 = self.up_convolution_2(up_1, down_3)
+        up_3 = self.up_convolution_3(up_2, down_2)
+        up_4 = self.up_convolution_4(up_3, down_1)
+
+        out = self.out(up_4)
+        return out
 
 
 #Augmentation
 image_transform = T.Compose([
-    T.Resize((512, 512)),
+    T.Resize((IMAGE_SIZE, IMAGE_SIZE)),
     # T.RandomHorizontalFlip(),
     # T.RandomVerticalFlip(),
     # T.RandomRotation(30),
     T.ToTensor()
 ])
 mask_transform = T.Compose([
-    T.Resize((512, 512), interpolation=Image.NEAREST),
+    T.Resize((IMAGE_SIZE, IMAGE_SIZE), interpolation=Image.NEAREST),
     # T.RandomHorizontalFlip(),
     # T.RandomVerticalFlip(),
     # T.RandomRotation(30),
@@ -156,6 +173,9 @@ optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 best_val_loss = float("inf")
 best_model_path = "models/unet_best.pth"
 
+train_losses = []
+val_losses = []
+
 # Train
 for epoch in range(EPOCHS):
     # Training
@@ -187,6 +207,9 @@ for epoch in range(EPOCHS):
             val_loss += loss.item() * images.size(0)
 
     avg_val_loss = val_loss / len(val_loader.dataset)
+    train_losses.append(avg_train_loss)
+    val_losses.append(avg_val_loss)
+
     if avg_val_loss < best_val_loss:
         best_val_loss = avg_val_loss
         torch.save(model.state_dict(), best_model_path)
@@ -210,13 +233,26 @@ img = images[0].permute(1, 2, 0).cpu().numpy()
 mask = masks[0].cpu().numpy()
 pred = preds[0].cpu().numpy()
 
+# Loss plot
+plt.figure(figsize=(8,6))
+plt.plot(range(1, EPOCHS+1), train_losses, label="Training Loss")
+plt.plot(range(1, EPOCHS+1), val_losses, label="Validation Loss")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.title("Training vs Validation Loss")
+plt.ylim(0, .66)
+plt.legend()
+plt.grid(True)
+plt.show()
+
+# Images plot
 plt.figure(figsize=(12,4))
 plt.subplot(1,3,1)
 plt.title("Image")
 plt.imshow(img)
 
 plt.subplot(1,3,2)
-plt.title("Ground Truth")
+plt.title("Mask")
 plt.imshow(mask, cmap="gray")
 
 plt.subplot(1,3,3)
